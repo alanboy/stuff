e87a23d54a8271d01abdd2b13cb27e4e0d3b3ba8
Alan Gonzalez <alanboy@alanboy.net>
2017-02-21 22:06:39 -0800

    add tmuxinator for tmux session management

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
 
-ln -s  `pwd`/tmux.conf ~/.tmux.conf
+sudo apt-get install tmuxinator
+
+export EDITOR='vim'
+
+if [ ! -f ~/.tmux.conf ]; then
+    ln -s  `pwd`/tmux.conf ~/.tmux.conf
+fi
+
+if [ ! -d ~/.tmuxinator/ ]; then
+    mkdir ~/.tmuxinator
+fi
+
+if [ ! -f ~/.tmuxinator/omegaup.yml ]; then
+    ln -s  `pwd`/omegaup.tmuxinator.yml ~/.tmuxinator/omegaup.yml
+fi
+
+if [ ! -f ~/.tmuxinator/enterpos.yml ]; then
+    ln -s  `pwd`/omegaup.enterpos.yml ~/.tmuxinator/enterpos.yml
+fi
+
