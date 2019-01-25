33d56ad7995e1fa33db7e7d96f334b342c74f949
Alan Gonzalez <alango@microsoft.com>
2019-01-25 12:23:34 -0800

    Vim blog post and new newpost script

---
+++ blog/_posts/newpost.cmd
@@ -0,0 +1,25 @@
+
+echo off
+
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
+For /f "tokens=1-2 delims=/: " %%a in ('time /t') do (set mytime=%%a:%%b:00)
+
+set filename=%mydate%.%1.markdown
+
+IF EXIST "%filename%" (
+    echo File already exists: %filename%
+    echo.
+) ELSE (
+    echo ---> %filename%
+    echo layout: post>> %filename%
+    echo title: "%1">> %filename%
+    echo date: %mydate% %mytime% -0800>> %filename%
+    echo categories: %1>> %filename%
+    echo --->> %filename%
+    echo.>> %filename%
+    echo.>> %filename%
+    git add %filename%
+    echo File created and added to source control: %filename%
+    echo.
+)
+
