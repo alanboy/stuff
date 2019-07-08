e8a7a67cc0e7611d4943e27efd42875843521729
Alan Gonzalez <alanboy@alanboy.net>
2019-07-08 16:09:59 -0700

    Don't hardcode the vim version

--- vim/install.sh
+++ vim/install.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
+PathToVim=$(find   /usr/share/vim  -regextype posix-extended -regex '.*vim[0-9]+')
 
 if [ ! -f ~/.vimrc ]; then
     ln -s  $PWD/linux.vim ~/.vimrc
 fi
 
-if [ ! -f /usr/share/vim/vim74/shared.vim ]; then
-    ln -s  $PWD/shared.vim /usr/share/vim/vim74/shared.vim
+if [ ! -f $PathToVim/shared.vim ]; then
+    ln -s  $PWD/shared.vim $PathToVim/shared.vim
 fi
 
-if [ ! -f /usr/share/vim/vim74/fzf.vim ]; then
-    ln -s  $PWD/fzf/plugin/fzf.vim /usr/share/vim/vim74/fzf.vim
+if [ ! -f $PathToVim/fzf.vim ]; then
+    ln -s  $PWD/fzf/plugin/fzf.vim $PathToVim/fzf.vim
 fi
 
 if [ ! -d ~/.vim/bundle ]; then
